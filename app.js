1ea539563b0657a2a891aff9bffeee4a786eeca5
nicodichino <nicolasdichino@gmail.com>
2023-05-29 23:12:40 -0300

    mensaje

--- app.js
+++ app.js
@@ -1,31 +1,38 @@
 const express = require('express');
+const passport = require('passport');
+const bodyParser = require('body-parser');
+const authRouter = require('./routes/auth');
+const booksRouter = require('./routes/books');
+const librariesRouter = require('./routes/libraries');
+require('./config/passport');
 
 const app = express();
+//Incluyo esta linea para que se haga el setting del numero de puerto
+//dinamico y se puede configurar el puerto a una variable enviroment definida
+//o que lo haga automaticamente en el puerto 3000 si no esta definida.
+const port = process.env.PORT || 3000;
 
-const sequelize = require ('./config/database');
+// Middleware
+app.use(bodyParser.json());
+app.use(passport.initialize());
 
-app.use(express.json());
-
-const librariesRouter = require('./routes/libraries');
-
-const booksRouter = require('./routes/books');
-
-const authRouter = require('./routes/auth');
-
-
-app.use('/libraries', librariesRouter);
-
-app.use('/books', booksRouter);
-
+// Rutas
 app.use('/auth', authRouter);
-
+app.use('/books', booksRouter);
+app.use('/libraries', librariesRouter);
 
 
 app.get('/', (req, res) => {
-    res.send('Bookstore API');
+  res.send('Bookstore API');
 });
 
-const port = 3000;
+// Middleware para manejo de errores
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: 'Internal server error' });
+});
+
+
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
